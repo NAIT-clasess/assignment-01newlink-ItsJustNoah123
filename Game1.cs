7001fad74278929c3ece3f665eac9ddc1174507b
ItsJustNoah123 <noahhemmelgarn204@hotmail.com>
2026-01-29 18:50:24 -0700

    Add Station texture and draw it; fix project files
    Update solution to reference Assignment_01.csproj and fix the csproj opening tag. Add Station.png to Content/Content.mgcb with texture importer/processor settings. Modify Game1: add _spaceStation field, load the 'Station' texture in LoadContent, and draw it in Draw using SpriteBatch. Also removed the early Exit() check in Update.

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,7 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Texture2D _spaceStation;
 
     public Game1()
     {
@@ -26,15 +27,12 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        _spaceStation = Content.Load<Texture2D>("Station");
         // TODO: use this.Content to load your game content here
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
-
         // TODO: Add your update logic here
 
         base.Update(gameTime);
@@ -43,7 +41,9 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
-
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(_spaceStation, Vector2.Zero, Color.White);
+        _spriteBatch.End();
         // TODO: Add your drawing code here
 
         base.Draw(gameTime);
